7e12680471765d95c6eee070afd6e273682f47e7
ryusei <kys0buchi624@gmail.com>
2018-09-27 20:03:46 +0900

    練習プログラムを作成

---
+++ UnityProject/Assets/NewBehaviourScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	public Text textfield;
+	private int count = 0;
+	
+	// Use this for initialization
+	void Start () {
+		textfield.text = "0";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	count = count + 1;
+	Debug.Log(count);
+	textfield.text = count.ToString();
+		
+	}
+}
